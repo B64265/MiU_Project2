bdff768eb4dba04f024e0f9f211306500701f641
Jeremy Goldman <jeremygoldman@fullsail.edu>
2013-05-23 07:12:55 -0500

    error support, prominent done button, save to local storage

--- Gold/js/main.js
+++ Gold/js/main.js
@@ -2,11 +2,23 @@ $('#home').on('pageinit', function(){
 	//code needed for home page goes here
 });	
 		
-$('#addItem').on('pageinit', function(){
+$('#reminder').on('pageinit', function(){
 
-		var myForm = $('#formId');
+		var myForm = $('#reminderForm'),
+			reminderErrorLink = $('#reminderErrorLink');
+		 
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
+				reminderErrorLink.click();
+				var html = '';
+				for(var key in validator.submitted){
+					var label = $('label[for^="'+ key +'"]').not('[generated]');
+					var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+					var fieldName = legend.length ? legend.text() : label.text();
+					html += '<li>' + fieldName +'</li>';
+				};
+				
+				$("#reminderErrors ul").html(html);
 			},
 			submitHandler: function() {
 		var data = myForm.serializeArray();
@@ -28,7 +40,41 @@ var getData = function(){
 
 };
 
+//Find value of radio button
+	function getSelectedRadio(){
+		
+		var radioButtons = document.forms[0].priority;
+		for(var i=0; i<radioButtons.length; i++){
+			
+			if(radioButtons[i].checked){
+				priorityValue = radioButtons[i].value;
+			}
+		}
+	}
+
+
 var storeData = function(data){
+		
+		var id = Math.floor(Math.random()*1000000);
+	
+		getSelectedRadio();
+		
+		//Form data into an object..
+		//Object properties has array with label and value.
+		
+		var item 				= {};
+		item.group 				= ["Group:", $('groups').value];
+		item.remindTitle 		= ["Reminder Title:", $('remindTitle').value];
+		item.dueDate			= ["Due Date:", $('due').value];
+		item.priority			= ["Priority:", priorityValue];
+		item.recurrence			= ["Recurrence:",$('recurrence').value];
+		item.description		= ["Description:", $('description').value];
+		
+		
+		//Save to Local Storage
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Reminder is set!");
+
 	
 }; 
 
